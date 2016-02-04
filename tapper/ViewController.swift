f1b67bc867ff4dd6ce6f1b241e3a6c06e7c342cf
Yogesh Kumar <yogeshkumar180592@gmail.com>
2016-02-04 23:22:16 +0530

    Added checks for text input and refactored the hidden states

--- tapper/ViewController.swift
+++ tapper/ViewController.swift
@@ -10,20 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Properties
+    var maxTaps = 0;
+    var currentTaps = 0;
+    
+    //Outlets
     @IBOutlet weak var logoImage: UIImageView!
     @IBOutlet weak var howManyTapsTxt: UITextField!
     @IBOutlet weak var playBtn: UIButton!
-    
     @IBOutlet weak var tapBtn: UIButton!
     @IBOutlet weak var tapsLabel: UILabel!
     
-    @IBAction func onPlayBtnPressed (sender:UIButton!){
-        logoImage.hidden = true
-        playBtn.hidden = true
-        howManyTapsTxt.hidden = true
-        tapBtn.hidden = false
-        tapsLabel.hidden = false
+    //Actions
+    @IBAction func onPlayBtnPressed (sender: UIButton!){
+        
+        if (howManyTapsTxt.text != nil && howManyTapsTxt.text != ""){
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0
+            
+            logoImage.hidden = true
+            playBtn.hidden = true
+            howManyTapsTxt.hidden = true
+
+            tapBtn.hidden = false
+            tapsLabel.hidden = false
+            
+            tapsLabel.text = "\(currentTaps) Taps"
+        }
     }
+    
+    @IBAction func onTapBtnPressed (sender: UIButton!) {
+        currentTaps++
+        tapsLabel.text = "\(currentTaps) Taps"
+    }
+    
+    
 
 }
 
